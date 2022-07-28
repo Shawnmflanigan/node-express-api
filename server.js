104df47a7e97b140c58beba014046aa80c47f962
Shawn Flanigan <shawn@kivo.io>
2022-07-28 16:03:30 -0700

    lots of work building the api

--- server.js
+++ server.js
@@ -3,32 +3,42 @@ const bodyParser = require("body-parser");
 const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
-const app = express();
+const server = () => {
+  const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-mongoose.Promise = global.Promise;
+  mongoose.Promise = global.Promise;
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("Successfully connected to the database");
-  })
-  .catch((err) => {
-    console.log("Could not connect to the database. Error...", err);
-    process.exit();
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to the database");
+    })
+    .catch((err) => {
+      console.log("Could not connect to the database. Error...", err);
+      process.exit();
+    });
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is running :D" });
   });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Server is running :D" });
-});
+  let PORT = 8080;
 
-let PORT = 8080;
+  require("./app/routes/app.routes.js")(app);
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+  if (!module.parent) {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  }
+};
+
+server();
+
+module.exports = server;
---
+++ test/app.controller.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../server");
+
+describe("GET /get-all", () => {
+  it("returns an empty array", (done) => {
+    request(app)
+      .get(`/get-all`)
+      .expect((res) => res.body.length.to.be(2), done());
+  });
+});
