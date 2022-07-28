7c38e1ead25b899179941400a18539b6a859c104
Shawn Flanigan <shawn@kivo.io>
2022-07-28 12:50:29 -0700

    add connection to mongodb

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Error...", err);
+    process.exit();
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Server is running :D" });
