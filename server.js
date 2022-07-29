5cc6c2d46d94f84ee0d8a26ac9f0db0778e65815
Shawn Flanigan <shawn@kivo.io>
2022-07-28 18:25:38 -0700

    add get all test

--- server.js
+++ server.js
@@ -3,40 +3,32 @@ const bodyParser = require("body-parser");
 const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
-const server = () => {
-  mongoose.Promise = global.Promise;
-  mongoose
-    .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-    })
-    .then(() => {
-      console.log("Successfully connected to the database");
-    })
-    .catch((err) => {
-      console.log("Could not connect to the database. Error...", err);
-      process.exit();
-    });
-
-  const app = express();
-
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.use(bodyParser.json());
-
-  app.get("/", (req, res) => {
-    res.json({ message: "Server is running :D" });
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Error...", err);
+    process.exit();
   });
 
-  let PORT = 8080;
+const app = express();
 
-  require("./app/routes/app.routes.js")(app);
-  if (!module.parent) {
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}`);
-    });
-  }
-};
+app.use(bodyParser.urlencoded({ extended: true }));
 
-server();
+app.use(bodyParser.json());
 
-module.exports = server;
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running :D" });
+});
+
+let PORT = 8080;
+
+require("./app/routes/app.routes.js")(app);
+module.exports = app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
--- test/app.controller.js
+++ test/app.controller.js
@@ -1,11 +1,22 @@
 const request = require("supertest");
 const app = require("../server");
 
-describe("GET /get-all", () => {
-  it("returns an empty array", (done) => {
+//Require the dev-dependencies
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("GET /get-all", function () {
+  it("returns an empty array", function (done) {
     request(app)
-      .get(`/get-all`)
-      .expect((res) => res.body.length.to.be(2))
-      .end(done);
+      .get("/get-all")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(0);
+        done();
+      });
   });
 });
